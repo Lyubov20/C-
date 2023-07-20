b630d898128c6f60753252fe18d66f0c7c6b1e91
Lyubov20 <isaevalyubov42@yandex.ru>
2023-07-20 16:26:53 +0500

    add задание4 Seminar_1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -37,38 +37,55 @@
 // 	3 -> Среда 
 // 5 -> Пятница
 
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1)
-{
-   Console.WriteLine("Понедельник"); 
-}
-else if (number == 2)
-{
-    Console.WriteLine("Вторник");
-}
-else if (number == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if (number == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if (number == 5)
-{
-    Console.WriteLine("Пятница");
-}
-else if (number == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if (number == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
-else
-{
-    Console.WriteLine("Такого дня нет");
-}
+// if (number == 1)
+// {
+//    Console.WriteLine("Понедельник"); 
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (number == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (number == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (number == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (number == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Такого дня нет");
+// }
+
+// 5. Напишите программу, которая на вход принимает одно
+//  число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int numberNeg = number*-1;
+
+// while(numberNeg <= number)
+// {
+//     Console.WriteLine(numberNeg);
+//     numberNeg = numberNeg + 1;
+// }
