0188dfc8a60133abb3eef132c2001b793f1352e4
Lyubov20 <isaevalyubov42@yandex.ru>
2023-07-20 16:19:00 +0500

    add задание3 Seminar_1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -17,17 +17,58 @@
 // a = 9, b = -3 -> да 
 // a = -3 b = 9 -> нет
 
-Console.WriteLine("Введите число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 == number2 * number2)
+// {
+//     Console.WriteLine("Да");
+// }
+// else 
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// 3. Напишите программу, которая будет выдавать название 
+// дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
 
 Console.WriteLine("Введите число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-if (number1 == number2 * number2)
+if (number == 1)
 {
-    Console.WriteLine("Да");
+   Console.WriteLine("Понедельник"); 
 }
-else 
+else if (number == 2)
 {
-    Console.WriteLine("Нет");
+    Console.WriteLine("Вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Такого дня нет");
 }
