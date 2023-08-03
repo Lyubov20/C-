99b43fa5e8b28aeca6b1c40b0c2c1853991ec657
Lyubov20 <isaevalyubov42@yandex.ru>
2023-08-03 14:08:29 +0500

    add homework4

---
+++ Seminar_4/homework4/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.Write("Введите число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите степень: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int Power()
+// {
+//     int res = 1;
+//     for (int i = 0; i < num2; i++)
+//     {
+//         res = res * num1;
+//     }
+//     return res;
+// }
+
+// Console.WriteLine();
+// Console.WriteLine(Power());
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numS = Convert.ToString(num);
+
+// int GetSum()
+// {
+//     int res = 0;
+//     int size = numS.Length;
+//     for (int i = 0; i < size; i++)
+//     {
+//         res += num % 10;
+//         num /= 10;
+//     }
+//     return res;
+// }
+
+// Console.WriteLine();
+// Console.WriteLine(GetSum());
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int [] array= new int[8];
+// GetArray();
+
+// void GetArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0,500);
+//         Console.Write(array[i]+" ");
+//     }
+// }
+
+
+
+
