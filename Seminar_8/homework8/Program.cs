36ee50e6335c6e0ecdfa8987589d6e07736afa65
Lyubov20 <isaevalyubov42@yandex.ru>
2023-08-31 14:28:35 +0500

    add homework8

--- Seminar_8/homework8/Program.cs
+++ Seminar_8/homework8/Program.cs
@@ -142,75 +142,172 @@
 // 18 20
 // 15 18
 
-Console.WriteLine("Первая размерность первого массива: ");
-int rowsOne = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Первая размерность первого массива: ");
+// int rowsOne = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Вторая размерность первого массива: ");
-int colomsOne = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Вторая размерность первого массива: ");
+// int colomsOne = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrixOne = new int[rowsOne,colomsOne];
+// int[,] matrixOne = new int[rowsOne,colomsOne];
 
-Console.WriteLine("Первая размерность второго массива: ");
-int rowsTwo = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Первая размерность второго массива: ");
+// int rowsTwo = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Вторая размерность второго массива: ");
-int colomsTwo = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Вторая размерность второго массива: ");
+// int colomsTwo = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrixTwo = new int[rowsTwo,colomsTwo];
+// int[,] matrixTwo = new int[rowsTwo,colomsTwo];
 
-if (colomsOne == rowsTwo)
-{
+// if (colomsOne == rowsTwo)
+// {
 
-int[,] matrixThree = new int[rowsTwo, colomsOne];
+// int[,] matrixThree = new int[rowsOne,colomsTwo];
 
-void CreateArray(int[,] array) 
-{ 
-    for (int i = 0; i < array.GetLength(0); i++) 
-    { 
-        for (int j = 0; j < array.GetLength(1); j++) 
-        { 
-            array[i,j] = new Random().Next(0,10); 
-        } 
-    } 
-} 
+// void CreateArray(int[,] array) 
+// { 
+//     for (int i = 0; i < array.GetLength(0); i++) 
+//     { 
+//         for (int j = 0; j < array.GetLength(1); j++) 
+//         { 
+//             array[i,j] = new Random().Next(0,10); 
+//         } 
+//     } 
+// } 
  
-void PrintArray(int[,] array) 
-{ 
-    for (int i = 0; i < array.GetLength(0); i++) 
-    { 
-        for (int j = 0; j < array.GetLength(1); j++) 
-        { 
-            Console.Write(array[i,j] + " "); 
-        } 
-        Console.WriteLine(); 
-    } 
-} 
+// void PrintArray(int[,] array) 
+// { 
+//     for (int i = 0; i < array.GetLength(0); i++) 
+//     { 
+//         for (int j = 0; j < array.GetLength(1); j++) 
+//         { 
+//             Console.Write(array[i,j] + " "); 
+//         } 
+//         Console.WriteLine(); 
+//     } 
+// } 
 
-void MatrixMulty(int[,] firstArray, int[,] secondArray, int[,] finalArray)
-{ 
-    for (int i = 0; i < finalArray.GetLength(0); i++) 
-    { 
-        for (int j = 0; j < finalArray.GetLength(1); j++) 
-        { 
-            int sum = 0;
-            for (int k = 0; k < secondArray.GetLength(1); k++)
-                {
-                    sum += firstArray[i, k] * secondArray[k, j];
-                }
-            finalArray[i, j] = sum;
-        } 
-    } 
-}
-CreateArray(matrixOne);
-PrintArray(matrixOne);
-Console.WriteLine();
-CreateArray(matrixTwo);
-PrintArray(matrixTwo);
-Console.WriteLine();
-MatrixMulty(matrixOne, matrixTwo, matrixThree);
-PrintArray(matrixThree);
-}
-else 
-{
-    Console.WriteLine("Введите размерности верно! Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы.");
-}
+// void MatrixMulty(int[,] firstArray, int[,] secondArray, int[,] finalArray)
+// { 
+//     for (int i = 0; i < finalArray.GetLength(0); i++) 
+//     { 
+//         for (int j = 0; j < finalArray.GetLength(1); j++) 
+//         { 
+//             int sum = 0;
+//             for (int k = 0; k < firstArray.GetLength(1); k++)
+//                 {
+//                     sum += firstArray[i, k] * secondArray[k, j];
+//                 }
+//             finalArray[i, j] = sum;
+//         } 
+//     } 
+// }
+// CreateArray(matrixOne);
+// PrintArray(matrixOne);
+// Console.WriteLine();
+// CreateArray(matrixTwo);
+// PrintArray(matrixTwo);
+// Console.WriteLine();
+// MatrixMulty(matrixOne, matrixTwo, matrixThree);
+// PrintArray(matrixThree);
+// }
+// else 
+// {
+//     Console.WriteLine("Введите размерности верно! Количество столбцов первой матрицы должно совпадать с количеством строк второй матрицы.");
+// }
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+// int[,,] array = new int[2, 2, 2];
+
+// void CreateArray(int[,,] matrix)
+// {
+//     int count = 10;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 matrix[j, k, i] = count;
+//                 count += 8;
+//             }
+//         }
+//     }
+// }
+
+// void PrintIndex(int[,,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+//             }
+//         }
+//     }
+// }
+
+// CreateArray(array);
+// PrintIndex(array);
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+// int elements = 4; 
+// int[,] array = new int[elements, elements];
+
+// void CreateSpiral(int[,] matrix)
+// {
+//     int i = 0;
+//     int j = 0;
+//     int temp = 1;
+
+//     while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+//         {
+//             matrix[i, j] = temp;
+//             temp++;
+//             if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+//                 j++;
+//             else if (i < j && i + j >= matrix.GetLength(0) - 1)
+//                 i++;
+//             else if (i >= j && i + j > matrix.GetLength(1) - 1)
+//                 j--;
+//             else
+//                 i--;
+//         }
+// }
+
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (matrix[i, j] < 10)
+//             {
+//                 Console.Write("0" + matrix[i, j]);
+//                 Console.Write(" ");
+//             }
+//             else 
+//             {
+//                     Console.Write(matrix[i, j] + " ");
+//              }
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// CreateSpiral(array);
+// PrintArray(array);
